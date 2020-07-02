edb5a684809a6d89d1050c0c2f9b146c62f0a38a
difu <difu@mac.com>
2020-07-02 18:00:41 +0200

    file io metrics

--- create_metrics_proc.sql
+++ create_metrics_proc.sql
@@ -8,6 +8,15 @@ create or replace NONEDITIONABLE procedure prometheus_metrics IS
         select con_id, username, service_name, count(*) as numer_of_sessions
         from v$session where type ='USER' group by username, con_id, service_name;
 
+
+--
+-- Statement adapted from http://dirknachbar.blogspot.com/2020/03/generating-prometheus-metrics-directly.html
+--
+
+    cursor c_fileio is
+        SELECT ddf.con_id, ddf.tablespace_name as tablespace_name, d.name as filename, f.phyblkrd as blocksread, f.phyblkwrt as blockswriten, f.phyblkrd + f.phyblkwrt as totalio 
+        FROM v$filestat f, v$datafile d, cdb_data_files ddf WHERE  d.file# = f.file# and ddf.file_id = d.file# ORDER BY f.phyblkrd + f.phyblkwrt DESC;
+
   
 begin
 owa.num_cgi_vars := 0;
@@ -24,6 +33,14 @@ htp.init();
     htp.print('# TYPE oracle_sessions gauge');
     for rec_sessions in c_sessions loop
         htp.print('oracle_sessions{con_id="' || rec_sessions.con_id || '", username="' || rec_sessions.username || '", service="' || rec_sessions.service_name || '"} ' || rec_sessions.numer_of_sessions);
+    end loop;
+
+    htp.print('# HELP oracle_fileio Metrics from v$filestat, v$datafile');
+    htp.print('# TYPE oracle_fileio counter');
+    for rec_fileio in c_fileio loop
+        htp.print('oracle_fileio{con_id="' || rec_fileio.con_id || '", tablespace_name="'||rec_fileio.tablespace_name||'",filename="'||rec_fileio.filename||'",type="blocksread"} ' || rec_fileio.blocksread);
+        htp.print('oracle_fileio{con_id="' || rec_fileio.con_id || '", tablespace_name="'||rec_fileio.tablespace_name||'",filename="'||rec_fileio.filename||'",type="blockswriten"} ' || rec_fileio.blockswriten);
+        htp.print('oracle_fileio{con_id="' || rec_fileio.con_id || '", tablespace_name="'||rec_fileio.tablespace_name||'",filename="'||rec_fileio.filename||'",type="totalio"} ' || rec_fileio.totalio);
     end loop;
 
 end;
